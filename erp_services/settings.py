2522e638a8434127901bebf4864300d012e13ef9
ViniciusOliveiraTechnik <vinicius.oliveira@grupotechnik.com.br>
2025-05-12 15:31:54 -0300

    allset

--- erp_services/settings.py
+++ erp_services/settings.py
@@ -5,10 +5,8 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = "postgresql://postgres:yJppOdJGpIKoFWGVomDBZdIOvNVlisOX@tramway.proxy.rlwy.net:55818/railway"
-
 DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
+    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'), conn_max_age=1800),
 }
 
 # Quick-start development settings - unsuitable for production
