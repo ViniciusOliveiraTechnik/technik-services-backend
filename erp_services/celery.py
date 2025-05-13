6a19da651032db468eff0583bcf373f0fa317425
ViniciusOliveiraTechnik <vinicius.oliveira@grupotechnik.com.br>
2025-05-13 10:25:06 -0300

    allset

--- erp_services/celery.py
+++ erp_services/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp_services.settings.production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erp_services.settings.development')
 
 app = Celery('erp_services')
 app.config_from_object('django.conf:settings', namespace='CELERY')
